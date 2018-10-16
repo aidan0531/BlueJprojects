c02e5d886650804b32cea3dc9e7b302979512d08
aidan0531 <aidanhy1@gmail.com>
2018-10-16 12:36:21 -0500

    o
    eeeeeeee

---
+++ MediaLib/MediaLib.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class MediaLib here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MediaLib
+{
+    // instance variables - replace the example below with your own
+        public static void main()
+    {
+        System.out.println("Welcome to your media library");
+        Song song1 = new Song();
+        System.out.println(song1);
+        
+        
+        //#16 
+        
+    }
+}
+
+
+
